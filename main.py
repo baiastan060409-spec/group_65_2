d284cb8b6038c3b8cda106b283387d992fa2b60a
baiastan060409-spec <baiastan060409@gmail.com>
2026-05-05 23:59:42 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-import flet as ft
+import fleat as ft
 
-
-def main_page(page: ft. Page):
-    page.title = 'Моё первое приложение'
-    page.theme_mode = ft. ThemeMode. LIGHT
-
-    hello_text = ft. Text(value='Hello world')
-
-    name_input = ft.TextField(label='Введите имя')
-
+def main_page(page: ft.Page):
     
-    def on_button_click(_):
-        #print(name_input.value)
-        #pass
-        name = name_input.value.strip()
+    hello_text = ft.Text(value='Hello world')
 
-        if name:
-            #print(name)
-            hello_text.color = None
-            hello_text.value = f'Hello {name}'
-            name_input.value = None
-        else:
-            #print('Error')
-            hello_text.value = ''
-            hello_text.color = ft.Colors.RED
-
-    name_input = ft.TextField(label='f', on_submit=on_button_click)
-    elevated_button = ft.ElevatedButton('SEND', icon=ft.Icons.SEND, on_click=on_button_click)
-    #text_button = ft.TextButton(text='SEND', icon=ft.Icons.SEND)
-    #icon_button = ft.IconButton(icon=ft.Icons.SEND)
-
-    
-    page.add(hello_text, name_input, elevated_button,)
+    page.add(hello_text)
 
 
 ft.app(main_page)
-#ft.app(main_page, view=ft.AppView.WEB_BROWSER)
-
