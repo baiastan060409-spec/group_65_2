ff470fc7af5a235b7a304c6733e4ea7e1aaf3a9e
Bayastan <baiastan060409@gmail.com>
2026-05-05 23:15:25 +0600

    group 65 2

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
-import fleat as ft
-
-def main_page(page: ft.Page):
-    
-    hello_text = ft.Text(value='Hello world')
-
-    page.add(hello_text)
+# import flet as ft 
 
 
-ft.app(main_page)
+# def main_page(page: ft.Page):
+#     page.title = 'Моё первое приложение'
+#     page.theme_mode = ft.ThemeMode.LIGHT
+
+#     hello_text = ft.Text(value='Helo world')
+
+#     name_input = ft.TextField(label='Введите имя')
+
+#     page.add(hello_text, name_input)
+#     def on_button_click(_):
+#         # print(name_input.value)
+#         # pass
+#         name = name_input.value.strip()
+
+#         if name:
+#             # print(name)
+#             # hello_text = 'sdfsdfsdf'
+#             hello_text.color = None
+#             hello_text.value = f'Hello {name}'
+#             name_input.value = None
+#         else:
+#             # print('Error')
+#             hello_text.value = 'ОШИБКА Введите имя'
+#             hello_text.color = ft.Colors.RED
+
+#         page.update()
+
+
+#     name_input = ft.TextField(label='Введите имя', on_submit=on_button_click)
+#     elevated_button = ft.ElevatedButton('SEND', icon=ft.Icons.SEND, on_click=on_button_click)
+#     # text_button = ft.TextButton(text='SEND', icon=ft.Icons.SEND)
+#     # icon_button = ft.IconButton(icon=ft.Icons.SEND)
+
+
+#     page.add(hello_text, name_input, elevated_button)
+
+
+# ft.app(main_page) 
+# # ft.app(main_page, view=ft.AppView.WEB_BROWSER)
+# # ft.app(main_page, view=ft.AppView.WEB_BROWSER)
+
+from datetime import datetime
+
+while True:
+    name = input("Введите имя (или 'выход'): ")
+
+    if name.lower() == "выход":
+        break
+
+    now = datetime.now()
+    time_str = now.strftime("%Y:%m:%d - %H:%M:%S")
+
+    print(f"{time_str} - Привет, {name}!")
